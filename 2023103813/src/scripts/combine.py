53849671fc822dc3a45155d0979f7fd2b4dc29ff
pbvrvnq <1446390293@qq.com>
2024-06-03 16:57:10 +0800

    initial commit of 2023103813

---
+++ 2023103813/src/scripts/combine.py
@@ -0,0 +1,19 @@
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn import model_selection
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+
+subtrainLabel = pd.read_csv('subtrainLabels.csv')
+subtrainfeature1 = pd.read_csv("3gramfeature.csv")
+subtrainfeature2 = pd.read_csv("imgfeature.csv")
+subtrain = pd.merge(subtrainfeature1, subtrainfeature2, on = 'Id')
+subtrain = pd.merge(subtrain, subtrainLabel, on='Id')
+labels = subtrain.Class
+subtrain.drop(["Class","Id"], axis = 1, inplace=True)
+subtrain = subtrain.values
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(subtrain,labels,test_size = 0.4)
+
+srf = RF(n_estimators = 500, n_jobs=-1)
+srf.fit(X_train,y_train)
+print srf.score(X_test,y_test)
